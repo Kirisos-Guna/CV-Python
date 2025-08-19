16cda83ed37fa4eaf95c96d6ac3a13416a0dfb74
Kirisos <m.gunamurugan2004@gmail.com>
2025-08-20 04:51:17 +0530

    Add intersection

--- bitwise.py
+++ bitwise.py
@@ -3,7 +3,16 @@ import cv2
 import matplotlib.pyplot as plt
 img = np.zeros((500,500),dtype=np.uint8)
 
-circle=cv2.circle(img,(250,250),250,(255,255,255),-1)
-plt.imshow(cv2.cvtColor(circle,cv2.COLOR_BGR2RGB))
+circle=cv2.circle(img.copy(),(250,250),250,(255,255,255),-1)
+rect = cv2.rectangle(img.copy(),(30,30),(470,470),(255,255,255),-1)
+# AND Operator - intersection
+intersection = cv2.bitwise_and(circle,rect,None)
+# OR Operator - union
+intersection = cv2.bitwise_or(circle,rect,None)
+# NOT Operator - complement
+intersection = cv2.bitwise_not(circle,rect,None)
+# XOR Operator - either one set or the other, but not in both.
+intersection = cv2.bitwise_xor(circle,rect,None)
+plt.imshow(cv2.cvtColor(intersection,cv2.COLOR_BGR2RGB))
 plt.axis("off")
 plt.show()
