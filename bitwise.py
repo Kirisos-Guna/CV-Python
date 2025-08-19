51e4fb84f833d7280e147a55206efbc58a5d33e9
Kirisos <m.gunamurugan2004@gmail.com>
2025-08-19 10:47:11 +0530

    Bitwise

---
+++ bitwise.py
@@ -0,0 +1,9 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+img = np.zeros((500,500),dtype=np.uint8)
+
+circle=cv2.circle(img,(250,250),250,(255,255,255),-1)
+plt.imshow(cv2.cvtColor(circle,cv2.COLOR_BGR2RGB))
+plt.axis("off")
+plt.show()
